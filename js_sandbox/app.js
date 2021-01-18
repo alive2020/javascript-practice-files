4d0c4c8383b2921b45e7f21b166a4dc584125edd
Ice K <alive.k001@gmail.com>
2021-01-18 15:17:14 +0900

    Add Book app/es5-es6 practice

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -43,28 +43,154 @@
 //   console.log(task);
 // });
 
-document.querySelector('form').addEventListener('submit',
-  function (e) {
-    const task = document.getElementById('task').value;
+// document.querySelector('form').addEventListener('submit',
+//   function (e) {
+//     const task = document.getElementById('task').value;
 
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-      tasks = [];
-    } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    tasks.push(task);
+//     let tasks;
+//     if (localStorage.getItem('tasks') === null) {
+//       tasks = [];
+//     } else {
+//       tasks = JSON.parse(localStorage.getItem('tasks'));
+//     }
+//     tasks.push(task);
 
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    alert('Task saved');
+//     localStorage.setItem('tasks', JSON.stringify(tasks));
+//     alert('Task saved');
 
-    e.preventDefault();
-  });
+//     e.preventDefault();
+//   });
 
 
-const tasks = JSON.parse(localStorage.getItem('tasks'));
+// const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-tasks.forEach(function (task) {
-  console.log(task);
-});
+// tasks.forEach(function (task) {
+//   console.log(task);
+// });
+
+
+
+// function Person(firstName, lastName, dob) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.birthday = new Date(dob);
+// }
+
+// Person.prototype.calculateAge = function () {
+//   const diff = Date.now() - this.birthday.getTime();
+//   const ageDate = new Date(diff);
+//   return Math.abs(ageDate.getUTCFullYear() - 1970);
+// }
+
+// Person.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// }
+
+// Person.prototype.isMarried = function (newLastName) {
+//   this.lastName = newLastName;
+// }
+
+// const brad = new Person('Brad', 'Doe', '9/10/1980');
+// console.log(brad);
+// console.log(brad.calculateAge());
+// console.log(brad.getFullName());
+
+// brad.isMarried('Smith');
+
+// console.log(brad.getFullName());
+
+
+// function Person(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
+
+// Person.prototype.greeting = function () {
+//   return `Hello there ${this.firstName} ${this.lastName}`;
+// }
+
+// const person1 = new Person('Jon', 'Doe');
+// console.log(person1.greeting());
+
+
+// function Customer(firstName, lastName, phone, membership) {
+//   Person.call(this, firstName, lastName);
+//   this.phone = phone;
+//   this.membership = membership;
+// }
+
+// Customer.prototype = Object.create(Person.prototype);
+// Customer.prototype.constructor = Customer;
+// Customer.prototype.greeting = function () {
+//   return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
+// }
+
+// const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+
+// console.log(customer1);
+// console.log(customer1.greeting());
+
+
+// Classes
+
+// class Person {
+//   constructor(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.birthday = new Date(dob);
+//   }
+//   greeting() {
+//     return `Hello there ${this.firstName} ${this.lastName}`;
+//   }
+//   calculateAge() {
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   }
+//   getsMarried(newLastName) {
+//     this.lastName = newLastName;
+//   }
+//   static addNumbers(x, y) {
+//     return x + y;
+//   }
+// }
+
+// const mary = new Person('Mary', 'Thompson', '11-13-1983');
+// mary.getsMarried('Williams');
+
+
+// console.log(mary);
+// console.log(Person.addNumbers(1, 2));
+
+
+// Subclasses 
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+}
+
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
+    this.phone = phone;
+    this.membership = membership;
+  }
+  static getMembershipCost() {
+    return 500;
+  }
+}
+
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
+
+console.log(john.greeting());
+
+console.log(Customer.getMembershipCost());
+
 
